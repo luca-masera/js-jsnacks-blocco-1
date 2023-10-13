4eb98a44c621f8c98e7b24fe3cd6861460315d44
luca-masera <lucamasera1995@gmail.com>
2023-10-13 15:21:54 +0200

    for + if frutta

--- array_lista/script-tuttiFrutti.js
+++ array_lista/script-tuttiFrutti.js
@@ -12,4 +12,23 @@ const fruttaFrigo = [
 ]
 
 fruttaFrigo.push('pesca')
+
 console.log(fruttaFrigo)
+
+let write;
+
+
+for(let i = 0; i < fruttaFrigo.length; i++){
+    let cocktail = i[fruttaFrigo];
+
+
+    if(cocktail==='cocomero'){
+        write = "Trovato! Devo solo preparare il cocktail."
+        console.log(write);
+        
+    } else if (cocktail !=='cocomero'){
+        write = "Oh no, devo uscire a comprare il cocomero!"
+        console.log(write);
+    }
+
+}
